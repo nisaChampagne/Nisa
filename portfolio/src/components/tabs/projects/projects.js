c7ebc20ab966bdb451561ad8c3ce6873dee12e65
Nisa Champagne <nchampag23@gmail.com>
2020-03-11 18:24:14 -0400

    project descriptions

--- portfolio/src/components/tabs/projects/projects.js
+++ portfolio/src/components/tabs/projects/projects.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PokeDex from "./pokedexnew.png";
 import Hogwarts from "./hogwarts.png";
-import Todo from "./reacttodonew.png";
+import Todo from "./grocerylist.png";
 import posed from "react-pose";
 
 import "./projects.scss";
@@ -21,6 +21,14 @@ export default function Projects() {
     <div className="projectsContainer">
       <div className="projects">
         <h3 className="projectH3">Hogwarts Sorting Hat Quiz</h3>
+        <div className="projectParaHolder">
+          <p className="projectParagraph">
+            This is a project I worked on as a stretch goal. It will sort you
+            into your Hogwarts house by your answers. I utilized class components to gain more practice. 
+            I was able to get practice with passing props with this project. 
+          </p>
+        </div>
+
         <Box>
           <img src={Hogwarts} alt="hogwarts-preview" className="pokedex" />
         </Box>
@@ -41,23 +49,37 @@ export default function Projects() {
       </div>
       <div className="projects">
         <h3 className="projectH3">React Todo List</h3>
+        <div className="projectParaHolder">
+          <p className="projectParagraph">
+            This was a project I did in my spare time after Computer Science course work. This list will utilize local storage
+            so when you refresh your page, your data will persist.
+          </p>
+        </div>
+
         <Box>
           <img src={Todo} alt="todo-preview" className="pokedex" />
         </Box>
         <div className="centeredBtns">
           <a
-            href="https://github.com/nisaChampagne/React-Todo"
+            href="https://github.com/nisaChampagne/CRA-Todo"
             className="projectBtn"
           >
             Github Link{" "}
           </a>
-          <a href="https://react-todo-4bm2326a5.now.sh/" className="projectBtn">
+          <a href="https://todo-hsy0vs7pb.now.sh/" className="projectBtn">
             Hosted
           </a>
         </div>
       </div>
       <div className="projects">
         <h3 className="projectH3">Simple PokeDex</h3>
+        <div className="projectParaHolder">
+          <p className="projectParagraph">
+            I have always been into Pokemon so this was a fun side project. This will utilize getting data from an open Pokemon API,
+            gathering the data in a response, then using that data to make a Pokedex like container. I have some future plans for this particular project.
+          </p>
+        </div>
+
         <Box>
           <img src={PokeDex} alt="pokedex-preview" className="pokedex" />
         </Box>
@@ -90,15 +112,11 @@ export default function Projects() {
           </a>
         </div>
       </div>
-        {/* <div className="projectList">
-          <ul>
-            <li>
-              <NavLink to="/drum" exact className="navLink_project">
-                Drum Kit
-              </NavLink>
-            </li>
-          </ul>
-        </div> */}
-      </div>
+      <div className="projectParaHolder">
+          <p className="projectParagraph">
+            This particular project was more practice with axios calls, getting and setting a token to localstorage, CRUD functionality, and displaying received data.
+          </p>
+        </div>
+    </div>
   );
 }
